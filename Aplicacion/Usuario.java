5e3619245e46175a89b3db2eaf82bb80a85cafb9
Rmolina09 <rubenivanmolinafigueroa@gmail.com>
2026-07-09 23:25:57 -0400

    Add files via upload

---
+++ Aplicacion/Usuario.java
@@ -0,0 +1,15 @@
+package Aplicacion_de_inscripcion_de_usuario;
+
+// Clase que representa un Usuario
+public class Usuario {
+    String id;
+    String nombre;
+    String email;
+
+    public Usuario(String id, String nombre, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+    }
+}
+
